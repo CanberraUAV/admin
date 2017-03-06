172f0a05fd1ba6d1cb12ebeb77cc85a54c33ac6f
Stephen <stephen_dade@hotmail.com>
2017-03-06 21:20:15 +1100

    Title tidying

--- conf.py
+++ conf.py
@@ -17,11 +17,11 @@ spelling_show_suggestions=True
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
-project = u'CanberraUAV Public Documentation'
-copyright = u'2013, CanberraUAV Team'
+project = u'CanberraUAV Public'
+copyright = u'2013, CanberraUAV'
 
-version = '1.0'
-release = '1.0'
+#version = '1.0'
+#release = '1.0'
 exclude_trees = ['_build']
 pygments_style = 'sphinx'
 
